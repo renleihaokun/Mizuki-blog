7bc52171d96e6a4eada30d44db3934863fa407de
renleihaokun <119118549+renleihaokun@users.noreply.github.com>
2025-12-02 23:59:31 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -21,8 +21,8 @@ const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UTC+8
 export const siteConfig: SiteConfig = {
 	title: "困",
-	subtitle: "One demo website吗？",
-	siteURL: "https://mizuki.mysqil.com/", // 请替换为你的站点URL，以斜杠结尾
+	subtitle: "demo吗？",
+	siteURL: "https://blog.renleihaokun.dpdns.org/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2025-12-02", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	timeZone: SITE_TIMEZONE,
