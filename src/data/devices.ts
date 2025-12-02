6c74cfad5c10e9f2f15c7f64bb8aea4275e6560e
renleihaokun <119118549+renleihaokun@users.noreply.github.com>
2025-12-02 23:02:11 +0800

    Add OPPO devices and update router details

--- src/data/devices.ts
+++ src/data/devices.ts
@@ -16,23 +16,23 @@ export type DeviceCategory = {
 };
 
 export const devicesData: DeviceCategory = {
-	OnePlus: [
+	OPPO: [
 		{
 			name: "OPPO Find X8 Pro",
 			image: "/images/device/x8p.png",
-			specs: "Gray / 16G + 512GB",
+			specs: "Blue / 16G + 512GB",
 			description: "Flagship performance, Hasselblad imaging, 80W SuperVOOC.",
 			link: "https://www.oppo.com/cn/smartphones/series-find-x/find-x8-pro/",
 		},
 	],
 	Router: [
 		{
-			name: "GL-MT3000",
-			image: "/images/device/mt3000.png",
+			name: "ZXHN E2631",
+			image: "/images/device/router_logo_ZXHNE2631.png",
 			specs: "1000Mbps / 2.5G",
 			description:
 				"Portable WiFi 6 router suitable for business trips and home use.",
-			link: "https://www.gl-inet.cn/products/gl-mt3000/",
+			link: "https://www.ztedevices.com/cn/product/ax3000/%e4%b8%ad%e5%85%b4ax3000%e5%b7%a1%e5%a4%a9%e7%89%88wi-fi-6%e8%b7%af%e7%94%b1/",
 		},
 	],
 };
