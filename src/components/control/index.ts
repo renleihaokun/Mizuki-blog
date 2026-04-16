73f84fc71c8986c07d22a719a8960ddbcebbd5d1
renleihaokun <chaogeshou520@163.com>
2026-04-16 18:25:31 +0800

    fix: update control index to export LightDarkSwitch

--- src/components/control/index.ts
+++ src/components/control/index.ts
@@ -14,6 +14,6 @@ export { default as FloatingTOC } from "./FloatingTOC.astro";
 export { default as LayoutSwitch } from "./LayoutSwitch.svelte";
 export { default as PageProgressBar } from "./PageProgressBar/PageProgressBar.astro";
 export { default as Pagination } from "./Pagination.astro";
-export { default as ThemeSwitch } from "./ThemeSwitch.svelte";
+export { default as ThemeSwitch } from "./LightDarkSwitch.svelte";
 
 // Note: Types are defined locally in their respective components
